d87e4863b55b99a85594dbe7a1b49ba500a705cc
dtr6210 <rialsdean@gmail.com>
2024-01-05 22:18:52 -0600

    completed lab2

--- Exercises/src/components/BitcoinRates.jsx
+++ Exercises/src/components/BitcoinRates.jsx
@@ -1,45 +1,53 @@
 import { useState, useEffect } from "react";
+import { useBitcoinPrice } from "../hooks/useBitcoinPrice";
 
 function BitcoinRates() {
-    const currencies = ['USD', 'AUD', 'NZD', 'GBP', 'EUR', 'SGD'];
-    const [currency, setCurrency] = useState(currencies[0]);
-    const [bitcoinPrice, setBitcoinPrice] = useState(null);
+  const currencies = ["USD", "AUD", "NZD", "GBP", "EUR", "SGD"];
+  const [currency, setCurrency] = useState(currencies[0]);
+  // const [bitcoinPrice, setBitcoinPrice] = useState(null);
 
-    useEffect(()=> {
-        console.log('Fetching Bitcoin Data');
-        let ignore = false;
+  const bitcoinPrice = useBitcoinPrice(currency);
 
-        fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}`)
-        .then(response => response.json())
-        .then(json => {
-            console.log('Response:', json);
-            if (!ignore) {
-                setBitcoinPrice(json.bitcoin[currency.toLowerCase()]);
-            }
-        });
+  // useEffect(()=> {
+  //     console.log('Fetching Bitcoin Data');
+  //     let ignore = false;
 
-        return () => {
-            ignore = true;
-            console.log('Cleanup efect');
-        };
-    }, [currency]);
+  //     fetch(`https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=${currency}`)
+  //     .then(response => response.json())
+  //     .then(json => {
+  //         console.log('Response:', json);
+  //         if (!ignore) {
+  //             setBitcoinPrice(json.bitcoin[currency.toLowerCase()]);
+  //         }
+  //     });
 
-    const options = currencies.map(curr => <option value={curr} key={curr}>{curr}</option>);
+  //     return () => {
+  //         ignore = true;
+  //         console.log('Cleanup efect');
+  //     };
+  // }, [currency]);
 
-    return (
-        <div>
-            <h3>Bitcoin Exchange Rate</h3>
-            <label>Choose currency:
-                <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
-                    {options}
-                </select>
-            </label>
-            <div>
-                <strong>Current Price: </strong> 
-                {bitcoinPrice} {currency}
-            </div>
-        </div>
-    );
+  const options = currencies.map((curr) => (
+    <option value={curr} key={curr}>
+      {curr}
+    </option>
+  ));
+
+  return (
+    <div>
+      <h3>Bitcoin Exchange Rate</h3>
+      <label>
+        Choose currency:
+        <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+          {options}
+        </select>
+      </label>
+      <div>
+        <strong>Current Price: </strong>
+        {bitcoinPrice} {currency}
+      </div>
+    </div>
+  );
 }
 
-export default BitcoinRates
+export default BitcoinRates;
