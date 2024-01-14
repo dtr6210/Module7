cf7b37842a6b85c19e29c8085900f3cab6c6eacd
dtr6210 <rialsdean@gmail.com>
2024-01-13 18:22:49 -0600

    completed lab5

--- Exercises/src/components/NavBar.jsx
+++ Exercises/src/components/NavBar.jsx
@@ -1,19 +1,42 @@
 import { NavLink } from "react-router-dom";
+import { AppBar, Box, Typography, Button } from "@mui/material";
+import { Toolbar } from "@mui/material";
 
+// export default function NavBar() {
+//   return (
+//     <nav className="NavBar">
+//       <ul>
+//         <li>
+//           <NavLink to="/">Home</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/login">Login</NavLink>
+//         </li>
+//         <li>
+//           <NavLink to="/bitcoin">Bitcoin Rates</NavLink>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
+
+// to include AppBar from MUI
 export default function NavBar() {
   return (
-    <nav className="NavBar">
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
-        <li>
-          <NavLink to="/login">Login</NavLink>
-        </li>
-        <li>
-          <NavLink to="/bitcoin">Bitcoin Rates</NavLink>
-        </li>
-      </ul>
-    </nav>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" style={{ background: "#991E00" }}>
+        <Toolbar>
+          <Button color="inherit" component={NavLink} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={NavLink} to="/login">
+            Login
+          </Button>
+          <Button color="inherit" component={NavLink} to="/bitcoin">
+            Bitcoin-Rates
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
